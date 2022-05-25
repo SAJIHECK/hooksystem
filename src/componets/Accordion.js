1d65fabcbdaebe25c4d470b94ef212dc337a8fee
sajeehck@gmail.com <sajeehck@gmail.com>
2022-05-25 23:20:45 +0530

    first commit

--- src/componets/Accordion.js
+++ src/componets/Accordion.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 import '../css/style.css';
 
 
 
 const Accordion =({item}) =>{
 
+   const [activeIndex,setActiveIndex]=useState(null);
+
+
     const onTitleClick=(index) => {
-        console.log('Title clicked',index);
+       setActiveIndex(index);
     };
 
     const renderedItems=item.map((ItemObj,index) =>{
+        const active =index === activeIndex ? 'active' : '';
         return (
         <React.Fragment key={ItemObj.tittle}>
-             <div className="title active"
+             <div className={`title ${active}`}
              onClick={()=>onTitleClick(index)}
              
              >
                  <i className="dropdown icon"></i>
                   {ItemObj.tittle}
             </div>
-            <div className="content active">
+            <div className={`content ${active}`}>
                  <p>{ItemObj.content}</p>
              </div>
         </React.Fragment>
