3a5aa1ed28c79280e8ea82dd7fbd8a2ab2c69c02
sajeeh <sajeehck@gmail.com>
2022-05-27 18:45:58 +0530

    secong

--- src/componets/search.js
+++ src/componets/search.js
@@ -1,7 +1,44 @@
-import React,{useState} from "react";
- 
+import React,{useState,useEffect} from "react";
+import axios from "axios"; 
+
 const Search= () =>{
 const[term,setTerm]=useState('');
+const[results,setResults]=useState([]);
+
+
+useEffect(() =>{
+   const search = async () => {
+     const{ data } = await axios.get('https://en.wikipedia.org/w/api.php',{
+       params:{
+            action: 'query',
+            list: 'search',
+            origin: '*',
+            format: 'json',
+            srsearch: term,
+       },
+   });
+   setResults(data.query.search);
+   };
+   if(term){
+    search();}
+},[term]);
+
+
+const renderedResults=results.map((result) => {
+return (
+    <div className="item">
+        <div className="content">
+            <div className="header">
+                {result.title}
+            </div>
+                {result.snippet}
+        </div>
+    </div>
+);
+});
+
+
+
 return (
 <div>
     <div className="ui form">
@@ -14,6 +51,7 @@ return (
             />
         </div>
     </div>
+    <div className="ui celled list" >{renderedResults}</div>
 </div>
 );
 };
